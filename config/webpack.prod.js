092045af1d50be15e82384eb43a52f2d98d0ae13
Paul Gilchrist <Paul Gilchrist>
2016-10-13 15:27:19 -0400

    Fix issue with webpack after package updates

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -23,7 +23,7 @@ module.exports = webpackMerge(commonConfig, {
   },
 
   plugins: [
-    new webpack.NoErrorsPlugin(),
+    //new webpack.NoErrorsPlugin(),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({ // https://github.com/angular/angular/issues/10618
       beautify: false,
