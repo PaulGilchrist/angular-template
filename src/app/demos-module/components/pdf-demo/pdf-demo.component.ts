536d627ff734fbe93264fa473c84aba6a40bbc18
Paul Gilchrist <paul.gilchrist@outlook.com>
2017-10-31 20:28:27 -0400

    Fix PDF Demo

---
+++ src/app/demos-module/components/pdf-demo/pdf-demo.component.ts
@@ -0,0 +1,102 @@
+import { Component, OnInit } from '@angular/core';
+import { PDF_OVERVIEW } from '../../data/pdf-overview.data';
+
+import * as _ from 'underscore';
+
+import * as pdfMake from 'pdfmake/build/pdfmake.js';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts.js';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+export interface DocImage {
+	name: string;
+	size: number;
+}
+
+@Component({
+	selector: 'pdf-demo',
+	templateUrl: './pdf-demo.component.html'
+})
+export class PdfDemoComponent implements OnInit {
+	overviewPdf: any = PDF_OVERVIEW; // Full docuemnt
+	_modalActive: Boolean = false;
+
+	ngOnInit() {
+		window['appInsights'].trackPageView('demos-module/pdf-demo.component');
+	}
+
+	viewModal(): void {
+		this._modalActive = true;
+	}
+
+	getImages(doc: any): DocImage[] {
+		const images: DocImage[] = [];
+		if(doc.images) {
+			const imageNames = Object.keys(doc.images);
+			for(let i=0; i<imageNames.length; i++) {
+				const image: any = {};
+				image.name = imageNames[i];
+				image.size = doc.images[image.name].length;
+				images.push(image);
+			}
+		}
+		// console.log(images);
+		return images;
+	}
+
+	removeImages(doc: any, maxSize: number): void {
+		// Get all images in the document and their sizes.
+		const images: DocImage[] = this.getImages(doc);
+		// Loop through content array looking for long images
+		for(let i=0; i<doc.content.length; i++) {
+			const imageName: string = doc.content[i].image;
+			if(imageName != null) {
+				// Find the matching image
+				const image: DocImage = _.findWhere(images, {name: imageName});
+				if(image.size > maxSize) {
+					// Remove image content since it is too large
+					// console.log('Removing image - ' + imageName + ' at index ' + i);
+					doc.content.splice(i, 1);
+					// The array is now one smaller so step back one
+					i--;
+				}
+			}
+		}
+	}
+
+	viewPdf(name: string): void {
+		const browser = this.getBrowser();
+		if(browser.substr(0,4)==='Edge') {
+			// Edge browser is not able to support large image sizes
+			this.removeImages(this.overviewPdf, 19964);
+		}
+		if((browser.substr(0,2)==='IE') || (browser.substr(0,4)==='Edge')) {
+			pdfMake.createPdf(this.overviewPdf).download('demo.pdf');
+		} else {
+			// View the PDF (temporarily Chrome & FireFox only)
+			pdfMake.createPdf(this.overviewPdf).open();
+		}
+		// Another option that we are not using is pdfMake.createPdf(this.overviewPdf).print(); //works in Chrome & FireFox only
+	}
+
+	getBrowser(): string {
+		const ua= navigator.userAgent;
+		let tem: RegExpMatchArray;
+		let M: RegExpMatchArray = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+		if(/trident/i.test(M[1])) {
+			tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+			return 'IE '+(tem[1] || '');
+		}
+		if(M[1]=== 'Chrome') {
+			tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+			if(tem!= null) {
+				return tem.slice(1).join(' ').replace('OPR', 'Opera');
+			}
+		}
+		M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+		if((tem= ua.match(/version\/(\d+)/i))!= null) {
+			M.splice(1, 1, tem[1]);
+		}
+		return M.join(' ');
+	}
+
+}
--- src/app/demos-module/demos.module.ts
+++ src/app/demos-module/demos.module.ts
@@ -13,9 +13,9 @@ import * as d3 from 'd3';
 import { Selection, select } from 'd3-selection';
 import { transition } from 'd3-transition';
 import * as dragula from 'dragula';
-// import 'pdfmake/build/pdfmake.js';
-// import 'pdfmake/build/vfs_fonts.js';
-
+import * as pdfMake from 'pdfmake/build/pdfmake.js';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts.js';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 import { BlockchainDemoComponent } from './components//blockchain/blockchain-demo.component';
 import { D3GraphComponent } from './components/d3-graph/d3-graph.component';
@@ -25,7 +25,7 @@ import { EditorDemoComponent } from './components/editor-demo/editor-demo.compon
 import { GraphDemoComponent } from './components/graph-demo/graph-demo.component';
 import { ModalDemoComponent } from './components/modal-demo/modal-demo.component';
 import { SubjectDemoComponent } from './components/subject-demo/subject-demo.component';
-// import { PdfDemoComponent } from './components/pdf-demo.component';
+import { PdfDemoComponent } from './components/pdf-demo/pdf-demo.component';
 
 import { BlockchainService } from './services/blockchain.service';
 
@@ -43,8 +43,8 @@ import {CKEditorModule} from '../../../node_modules/ng2-ckeditor';
 		EditorDemoComponent,
 		GraphDemoComponent,
 		ModalDemoComponent,
+		PdfDemoComponent,
 		SubjectDemoComponent
-		// PdfDemoComponent,
 	], // directives, components, and pipes owned by this NgModule
 	imports: [
 		CKEditorModule,
@@ -57,7 +57,7 @@ import {CKEditorModule} from '../../../node_modules/ng2-ckeditor';
 			{ path: 'drag', component: DragDemoComponent },
 			{ path: 'editor', component: EditorDemoComponent },
 			{ path: 'graph', component: GraphDemoComponent },
-			// { path: 'pdf', component: PdfDemoComponent },
+			{ path: 'pdf', component: PdfDemoComponent },
 			// Lazy Loading
 			{ path: 'floor', loadChildren: './floor-module/floor.module#FloorModule' },
 			{ path: 'subject', component: SubjectDemoComponent },
