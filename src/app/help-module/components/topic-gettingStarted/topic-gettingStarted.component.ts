bb663a8d7d4d79eb602b1886a10bb5ca32d41214
Paul Gilchrist <paul.gilchrist@outlook.com>
2019-11-14 08:36:20 -0500

    minor UI changes to better support mobile

--- src/app/help-module/components/topic-gettingStarted/topic-gettingStarted.component.ts
+++ src/app/help-module/components/topic-gettingStarted/topic-gettingStarted.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-topic-getting-started',
+    styleUrls: ['./topic-gettingStarted.component.css'],
     templateUrl: './topic-gettingStarted.component.html'
 })
 export class TopicGettingStartedComponent { }
--- src/app/help-module/components/topic-vstsDeploy/topic-vstsDeploy.component.ts
+++ src/app/help-module/components/topic-vstsDeploy/topic-vstsDeploy.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-topic-vsts-deploy',
+    styleUrls: ['./topic-vstsDeploy.component.css'],
     templateUrl: './topic-vstsDeploy.component.html'
 })
 export class TopicVstsDeployComponent { }
