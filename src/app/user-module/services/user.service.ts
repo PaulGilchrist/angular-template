f5ef0dd5752976ac4746c7bf65cf9082c6dcc4e3
Paul Gilchrist <paul.gilchrist@outlook.com>
2020-04-24 15:08:08 -0400

    Add retry strategy - Handle 429 error

--- src/app/user-module/services/user.service.ts
+++ src/app/user-module/services/user.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, combineLatest, throwError as observableThrowError, Observable, of } from 'rxjs';
-import { catchError, map, retry, tap } from 'rxjs/operators';
+import { catchError, map, retryWhen, tap } from 'rxjs/operators';
 
+import { genericRetryStrategy } from './generic-retry-strategy';
 import { environment } from '../../../environments/environment';
 import { Address } from '../models/address.model';
 import { State } from '../models/state.model';
@@ -15,13 +16,13 @@ export class UserService {
     private users = new BehaviorSubject<User[]>([]);
     users$ = this.users.asObservable();
 
-    private _lastStateDataRetreivalTime: number; // Time when user data was last retireved from the remote souce
-    private _lastUserDataRetreivalTime: number; // Time when user data was last retireved from the remote souce
+    private lastStateDataRetreivalTime: number; // Time when user data was last retireved from the remote souce
+    private lastUserDataRetreivalTime: number; // Time when user data was last retireved from the remote souce
 
     constructor(private http: HttpClient) {}
 
     public getUsers(force: boolean = false): Observable<User[]> {
-        if (force || this.users.getValue().length === 0 || Date.now() - this._lastUserDataRetreivalTime > environment.dataCaching.userData) {
+        if (force || this.users.getValue().length === 0 || Date.now() - this.lastUserDataRetreivalTime > environment.dataCaching.userData) {
             // For the purpose of this demo not having a real backend API, we will only get from JSON file when nothing exists in localStorage
             const usersJson = localStorage.getItem('users');
             if (usersJson) {
@@ -30,16 +31,15 @@ export class UserService {
                 console.log(`GET users`);
             } else {
                 combineLatest([
-                    this.http.get<Address[]>(environment.apiUrl + 'addresses.json'),
-                    this.http.get<User[]>(environment.apiUrl + 'users.json')
+                    this.http.get<Address[]>(environment.apiUrl + 'addresses.json').pipe(retryWhen(genericRetryStrategy())),
+                    this.http.get<User[]>(environment.apiUrl + 'users.json').pipe(retryWhen(genericRetryStrategy()))
                 ]).pipe(
-                    retry(3),
                     map(([addresses, users]) => {
                         users.forEach(user => user.addresses = addresses.filter(a => user.addressIds.includes(a.id)));
                         return users;
                     }),
                     tap((users: User[]) => {
-                        this._lastUserDataRetreivalTime = Date.now();
+                        this.lastUserDataRetreivalTime = Date.now();
                         // Caller can subscribe to users$ to retreive the users any time they are updated
                         this.users.next(users);
                         console.log(`GET users`);
@@ -52,11 +52,11 @@ export class UserService {
     }
 
     public getStates(force: boolean = false): Observable<State[]> {
-        if (force || this.states.getValue().length === 0 || Date.now() - this._lastStateDataRetreivalTime > environment.dataCaching.defaultLong) {
+        if (force || this.states.getValue().length === 0 || Date.now() - this.lastStateDataRetreivalTime > environment.dataCaching.defaultLong) {
             this.http.get(environment.apiUrl + 'states.json').pipe(
-                retry(3),
+                retryWhen(genericRetryStrategy()),
                 tap((states: State[]) => {
-                    this._lastStateDataRetreivalTime = Date.now();
+                    this.lastStateDataRetreivalTime = Date.now();
                     // Caller can subscribe to states$ to retreive the states any time they are updated
                     this.states.next(states);
                     console.log(`GET states`);
